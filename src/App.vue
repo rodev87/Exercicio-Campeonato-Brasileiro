c03c20b3c2978d416ef9e9120abc1fc0273222be
Rodrigo Lopez de Oliveira <rodrigoliveira.dev@gmail.com>
2022-03-18 13:46:09 -0300

    menu ok

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,61 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <div>
+      <v-app-bar color="green darken-1" dense dark>
+          <v-app-bar-nav-icon @click="drawer = true">
+
+          </v-app-bar-nav-icon>
+          <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
+
+      </v-app-bar>
+          <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="green darken-3"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer-field</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Classificação</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    </div>
+    <v-main>
+      <v-container>
+        Teste
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+      drawer: false,
+      group: null,
+    }),
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
