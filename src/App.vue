d26400cd65f9fe5ee6893da172d85fa42a47d6d7
Rodrigo Lopez de Oliveira <rodrigoliveira.dev@gmail.com>
2022-03-18 15:46:39 -0300

    menu componentizado na pasta shared

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,20 @@
 <template>
   <v-app>
-    <div>
-      <v-app-bar color="green darken-1" dense dark>
-          <v-app-bar-nav-icon @click="drawer = true">
-
-          </v-app-bar-nav-icon>
-          <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
-
-      </v-app-bar>
-          <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="green darken-3"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-soccer-field</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-soccer</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Classificação</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    </div>
-
     <v-main>
+      <AppTopBar/>
       <Inicio/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import AppTopBar from './components/shared/AppTopBar'
 import Inicio from './components/Inicio'
 
 export default {
   name: 'App',
-  data: () => ({
-      drawer: false,
-      group: null,
-    }),
     components: {
+      AppTopBar,
       Inicio
     }
 }
---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+      <v-app-bar color="green darken-1" dense dark>
+          <v-app-bar-nav-icon @click="drawer = true">
+
+          </v-app-bar-nav-icon>
+          <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
+
+      </v-app-bar>
+          <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="green darken-3"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer-field</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Classificação</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppTopBar',
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+}
+</script>
